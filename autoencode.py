36dc4d5ab0899adad38aca3aaea730b4055ef8d2
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-04-11 02:55:52 -0400

    again

--- autoencode.py
+++ autoencode.py
@@ -52,52 +52,96 @@ y_train = tf.expand_dims(y_train, axis=-1)
 
 print("New shapes: ", np.shape(x_train), " ", np.shape(y_train))
 
-# The encoding process
-input_img = Input(shape=NET_INPUT) 
+# # The encoding process
+# input_img = Input(shape=NET_INPUT) 
 
-############
-# Encoding #
-############
+# ############
+# # Encoding #
+# ############
 
-# Conv1 #
-x = Conv2D(filters = 16, kernel_size = (3, 3), activation='relu', padding='same')(input_img)
-x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
+# # Conv1 #
+# x = Conv2D(filters = 16, kernel_size = (3, 3), activation='relu', padding='same')(input_img)
+# x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
 
-# Conv2 #
-x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D(pool_size = (2, 2), padding='same')(x) 
+# # Conv2 #
+# x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
+# x = MaxPooling2D(pool_size = (2, 2), padding='same')(x) 
 
-# Conv 3 #
-x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
+# # Conv 3 #
+# x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
+# encoded = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
 
-# Note:
-# padding is a hyper-arameter for either 'valid' or 'same'. 
-# "valid" means "no padding". 
-# "same" results in padding the input such that the output has the same length as the original input.
+# # Note:
+# # padding is a hyper-arameter for either 'valid' or 'same'. 
+# # "valid" means "no padding". 
+# # "same" results in padding the input such that the output has the same length as the original input.
 
-############
-# Decoding #
-############
+# ############
+# # Decoding #
+# ############
 
-# DeConv1
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
+# # DeConv1
+# x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+# x = UpSampling2D((2, 2))(x)
 
-# DeConv2
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
+# # DeConv2
+# x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+# x = UpSampling2D((2, 2))(x)
 
-# Deconv3
-x = Conv2D(16, (3, 3), activation='relu')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+# # Deconv3
+# x = Conv2D(16, (3, 3), activation='relu')(x)
+# x = UpSampling2D((2, 2))(x)
+# decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 
-# Declare the model
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+# # Declare the model
+# autoencoder = Model(input_img, decoded)
+# autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
+inputs = Input(NET_INPUT)
+conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+drop4 = Dropout(0.5)(conv4)
+pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+drop5 = Dropout(0.5)(conv5)
+
+up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+merge6 = concatenate([drop4,up6], axis = 3)
+conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+merge7 = concatenate([conv3,up7], axis = 3)
+conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+merge8 = concatenate([conv2,up8], axis = 3)
+conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+merge9 = concatenate([conv1,up9], axis = 3)
+conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+model = Model(input = inputs, output = conv10)
+
+model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Train the model
 autoencoder.fit(x_train, y_train,
